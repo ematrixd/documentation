ce2ae21835e08407a11a0585a99f0d3c8c82028d
ematrixd <e.matrixd@ematrixd.ru>
2024-04-22 01:45:32 +0300

    test12

--- autocommit.sh
+++ autocommit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Путь к вашему репозиторию
-REPO_PATH=/путь/к/вашему/репозиторию
+REPO_PATH=/home/matrix/Documents/documentation/
 
 # Переход в директорию с репозиторием
 cd $REPO_PATH
@@ -11,13 +11,10 @@ git add .
 
 # Проверка наличия изменений
 if [[ -n "$(git status --porcelain)" ]]; then
-    # Настройка пользователя для коммита
-    git config --local user.email "e.matrixd@e.ematrixd.ru"
-    git config --local user.name "ematrixd"
 
     # Коммит изменений
     git commit -m "autocommit: change doc file" || exit 0
 
     # Отправка изменений в удаленный репозиторий
-    git push origin main
+    git push
 fi
