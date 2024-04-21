8fa11617a1843afbe8e16d2a78bff4dc824f7ba3
ematrixd <e.matrixd@ematrixd.ru>
2024-04-22 02:09:04 +0300

    autocommit: change doc file

---
+++ JavaScript/NextJs/linkLearn/link.md
@@ -0,0 +1,2 @@
+# Ссылка на документацию Next.js
+[Next.js официальная документация](https://nextjs.org/docs/getting-started/installation)
--- inotifywait.sh
+++ inotifywait.sh
@@ -4,12 +4,15 @@
 DOCUMENTATION_DIR=/home/matrix/Documents/documentation/
 
 # Переход в директорию documentation
-cd $DOCUMENTATION_DIR
+cd "$DOCUMENTATION_DIR" || exit
 
-# Отслеживание изменений в директории для файлов с расширением .md, включая вложенные директории
-inotifywait -m -r --monitor -e modify,create,delete --include '\.md$' $DOCUMENTATION_DIR |
-while read path action file; do
-    echo "File '$file' $action in directory $path"  # Выводим сообщение о действии
-    # Запускаем скрипт для автоматического коммита
-    /home/matrix/Documents/documentation/autocommit.sh
+# Отслеживание изменений в директории для всех файлов, включая вложенные директории
+inotifywait -m -r --monitor -e modify,create,delete "$DOCUMENTATION_DIR" |
+while read -r path action file; do
+    # Проверяем, что файл имеет расширение .md
+    if [[ "$file" == *.md ]]; then
+        echo "File '$file' $action in directory $path"  # Выводим сообщение о действии
+        # Запускаем скрипт для автоматического коммита
+        /home/matrix/Documents/documentation/autocommit.sh
+    fi
 done
