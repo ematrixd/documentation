f7ebc595bc3ed034cbcdcbf3fa2b9431224e3959
ematrixd <e.matrixd@ematrixd.ru>
2024-04-22 02:03:43 +0300

    autocommit: change doc file

---
+++ JavaScript/NextJs/learnFramework/createApp.md
@@ -0,0 +1,8 @@
+# Создать приложение Next.js
+```javascript
+npx create-next-app@latest
+```
+# В дирректории с приложением устанновить нужные зависимости
+```javascript
+npm install next@latest react@latest react-dom@latest
+```
--- inotifywait.sh
+++ inotifywait.sh
@@ -6,8 +6,8 @@ DOCUMENTATION_DIR=/home/matrix/Documents/documentation/
 # Переход в директорию documentation
 cd $DOCUMENTATION_DIR
 
-# Отслеживание изменений в директории для файлов с расширением .md
-inotifywait -m -r -e modify,create,delete --include '\.md$' $DOCUMENTATION_DIR |
+# Отслеживание изменений в директории для файлов с расширением .md, включая вложенные директории
+inotifywait -m -r --monitor -e modify,create,delete --include '\.md$' $DOCUMENTATION_DIR |
 while read path action file; do
     echo "File '$file' $action in directory $path"  # Выводим сообщение о действии
     # Запускаем скрипт для автоматического коммита
